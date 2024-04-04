ed5868ce71109ff011c2ad54c727f8743a0c8031
Alessandro Huayta Ecos <73677205+Alessndro19@users.noreply.github.com>
2024-04-03 19:53:25 -0500

    Add files via upload

---
+++ Ejemplo2.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Proyecto2;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author LAB-USR-ICA
+ */
+public class Ejemplo2 {
+    public static void main(String[] args) {
+        //Variable de Entrada
+        double area, radio;
+        
+        Scanner scan = new Scanner(System.in);
+        
+        //Solicitar Datos de Entrada
+        
+        System.out.println("Ingrese el Area de un circulo: ");
+        area = scan.nextDouble();
+        
+        System.out.println("Ingrese el radio de un circulo: ");
+        radio = scan.nextDouble();
+        
+        //Proceso
+        
+        
+        math
+    }
+}
